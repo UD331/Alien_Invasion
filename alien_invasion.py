a60dafa2638e9a585e4bb30a94fb679bf7984635
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2021-10-02 23:54:06 -0400

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,12 +2,18 @@ import sys
 
 import pygame
 
+from Settings import Settings
+
 def run_game():
-    # Inititalize game and create a screen object
+    # Inititalize pygame, settings and screen object.
     pygame.intit()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    # Set the background color
+    bg_color = (230, 230, 230)
+    
     # Start the main loop for the game
     while True:
 
@@ -15,7 +21,9 @@ def run_game():
         for event on pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-
+        # Redraw the screen during each pass through the loop
+        screen.fill(ai_settings.bg_color) 
+               
         # Make the most recently drawn screen visible
         pygame.display.flip()
 
