4aa8584b4e977f101491db8567b1dbdbcc0b88d9
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2021-10-13 02:05:17 -0400

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
 import pygame
+from pygame.sprite import Sprite
 
 from settings import Settings
 from ship import Ship
@@ -12,8 +13,10 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # Make a Ship
-    ship = Ship(screen)
-
+    ship = Ship(ai_settings, screen)
+    # Make a group to store bullets in
+    bullets = Group()
+    
     # Set the background color
     bg_color = (230, 230, 230)
 
@@ -21,9 +24,13 @@ def run_game():
     while True:
 
         # Watch for keyboard and mouse events.
-        gf.check_events()
+        gf.check_events(ai_settings, screen, ship, bullets)
 
+        ship.update()
+
+        gf.update_bullets(bullets)
+        
         # Update images on the screen and flip to the new screen
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
