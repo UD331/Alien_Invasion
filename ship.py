9a47261a22480a9d45ba39404f5fc508cf538da5
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2021-10-13 02:06:28 -0400

    Update ship.py

--- ship.py
+++ ship.py
@@ -2,10 +2,10 @@ import pygame
 
 class Ship():
 
-    def _init_(self, screen):
+    def _init_(self, screen, ai_settings):
         """Inititalize the ship and set its starting position."""
         self.screen = screen
-
+        self.ai_settings = ai_settings
         # Load the ship image and get its rect.
         self.image = pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
@@ -15,7 +15,26 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
-        def blitme(self):
-            """Fraw the ship at its current location."""
-            self.screen.blit(self.image, self.rect)
+        # Store a decimal value for the ship's center.
+        self.center = float(self.rect.centerx)
+        
+        # Movement flags
+        self.moving_right = False
+        self.moving_left = False
+        
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        # Update the ship's center value, not the rect.
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += sel.ai_settings.ship_speed_factor
+
+        if self.moving_left and self.rect.left > 0:
+            self.center -= sel.ai_settings.ship_speed_factor
+
+        # Update rect object from self.center
+        self.rect.centerx = self.center
+                            
+    def blitme(self):
+        """Fraw the ship at its current location."""
+        self.screen.blit(self.image, self.rect)
             
