19d7ac5b65d71d617bce626b37298baf2fa8fa2a
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2021-10-13 02:06:07 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -8,4 +8,13 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
-        
+
+        # Bullet Settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
+
+        # Ship settings
+        self.ship_speed_factor = 1.5
