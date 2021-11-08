eba0d9c92cf41cc94c9bae8683b9aff23bae778c
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2021-11-07 22:15:33 -0500

    Update alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,7 +1,7 @@
 import pygame
 from pygame.sprite import Sprite
-from alien import Alien
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 import game_functions as gf
 
@@ -12,6 +12,9 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # Create an instance to store game statistics
+    stats = GameStats(ai_settings)
 
     # Make a Ship, a group of bullets, and a group of aliens
     ship = Ship(ai_settings, screen)
@@ -27,14 +30,15 @@ def run_game():
     # Start the main loop for the game
     while True:
 
-        # Watch for keyboard and mouse events.
         gf.check_events(ai_settings, screen, ship, bullets)
 
-        ship.update()
+        if stats.game_active:
+            ship.update()
 
-        gf.update_bullets(bullets)
+            gf.update_bullets(ai_settings, stats, screen, ship, aliens, bullets)
 
-        # Update images on the screen and flip to the new screen
+            gf.update_aliens(ai_settings, ship, aliens)
+
         gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 run_game()
