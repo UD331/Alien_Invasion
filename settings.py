0581fc495ab4107608d91483868544717e682c50
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2021-11-11 16:59:18 -0500

    Update settings.py

--- settings.py
+++ settings.py
@@ -10,18 +10,32 @@ class Settings():
         self.bg_color = (230, 230, 230)
 
         # Bullet Settings
-        self.bullet_speed_factor = 3
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         self.bullets_allowed = 3
 
         # Ship settings
-        self.ship_speed_factor = 1.5
         self.ship_limit = 3
 
         # Alien settings
-        self.alien_speed_factor = 1
         self.fleet_drop_speed = 10
+
+        # How quickly the game speeds up.
+        self.speedup_scale = 1.1
+
+        self.initialize_dynamic_settings()
+
+    def initialize_dynamic_settings(self):
+
+        self.ship_speed_factor = 1.5
+        self.bullet_speed_factor = 3
+        self.alien_speed_factor = 1
         # Fleet direction 0f 1 represents right; -1 represents left.
         self.fleet_direction = 1
+
+    def increase_speed(self):
+
+        self.ship_speed_factor *= self.speedup_scale
+        self.bullet_speed_factor *= self.speedup_scale
+        self.alien_speed_factor *= self.speedup_scale 
