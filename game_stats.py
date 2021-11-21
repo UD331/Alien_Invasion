0e5c15f5d2e4dee327a951720004be018011fcfe
Udayan Pandey <63406706+UD331@users.noreply.github.com>
2021-11-21 17:44:39 -0500

    Update game_stats.py

--- game_stats.py
+++ game_stats.py
@@ -8,6 +8,11 @@ class GameStats():
         # Start ALiens Invasion in an inactive state.
         self.game_active = False
 
+        # Highscore should never be reset.
+        self.high_score = 0
+
     def reset_stats(self):
 
         self.ships_left = self.ai_settings.ship_limit
+        self.score = 0
+        self.level = 1
